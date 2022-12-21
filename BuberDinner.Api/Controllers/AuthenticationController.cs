b9e4f06d687d4accd164bb9ae972736a4ba0da28
Chris Watia <cwatia@seaburysolutions.com>
2022-12-21 12:13:24 +0300

    Implemented auth response and dependency injection

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -1,18 +1,51 @@
 using Microsoft.AspNetCore.Mvc;
 using BuberDinner.Contracts.Authentication;
+using BuberDinner.Infrastructure.Services.Authentication;
 
 namespace BuberDinner.Api.Controllers;
 
 [ApiController]
 [Route("auth")]
 public class AuthenticationController : ControllerBase{
+    private readonly IAuthenticationService _authenticationService;
+
+    //Constructor
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
     [HttpPost("register")]
     public IActionResult Register(RegisterRequest request){
-        return Ok(request);
+        var authResult = _authenticationService.Register(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            request.Password);
+
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token);
+
+        return Ok(response);
     }
 
     [HttpPost("login")]
     public IActionResult Login(LoginRequest request){
-        return Ok(request);
+        var authResult = _authenticationService.Login(
+            request.Email,
+            request.Password);
+
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token);
+
+        return Ok(response);
     }
 }
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,13 +1,32 @@
+using BuberDinner.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 {
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+
+    //Register App dependency Injection
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
+
     builder.Services.AddControllers();
 }
 
 
 var app = builder.Build();
 {
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
