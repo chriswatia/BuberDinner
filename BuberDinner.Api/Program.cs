4582f3d7aa7e79a051b3813e7fc4633ca9b07af4
Chris Watia <cwatia@seaburysolutions.com>
2022-12-27 22:33:22 +0300

    Mappings

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,24 +1,15 @@
-using BuberDinner.Api.Common.Errors;
+using BuberDinner.Api;
 using BuberDinner.Infrastructure;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 {
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-
-    //Register App dependency Injection
+    //Register Each layer dependency Injection
     builder.Services
+        .AddPresentation()
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
-
-    builder.Services.AddControllers();
-
-    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
-}
+   }
 
 
 var app = builder.Build();
